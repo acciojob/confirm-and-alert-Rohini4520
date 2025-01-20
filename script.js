eb381fc59863201d865cc0fc8bcbf7d5d590e694
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-01-20 22:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the button element
+const button = document.getElementById("proceedButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", () => {
+    // Show the confirmation dialog
+    if (confirm("Do you want to proceed?")) {
+        // If OK is clicked
+        alert("You clicked OK. Proceeding...");
+    } else {
+        // If Cancel is clicked
+        alert("You clicked Cancel. Exiting...");
+    }
+});
